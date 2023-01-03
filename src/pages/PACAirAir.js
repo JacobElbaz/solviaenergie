bac16ae83d4667c1abf1eec1079168d97b4c1794
JacobElbaz <jacobel@post.bgu.ac.il>
2023-01-03 14:39:49 +0200

    PAC

--- src/pages/PACAirAir.js
+++ src/pages/PACAirAir.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Button, Container, Typography } from '@mui/material'
 
 function PACAirAir() {
   return (
-    <div>PACAirAir</div>
+    <Container>
+      <Typography variant='h3' color='primary' fontWeight='bold' textAlign='center' className='my-5'>Pompe à chaleur Air/Air</Typography>
+      <Typography variant='h4' color='primary'>Tout savoir sur la pompe à chaleur Air-Air</Typography>
+      <Typography>La pompe à chaleur air-air utilise les calories de l’air, une énergie gratuite, pour chauffer efficacement et à moindre coût votre logement. Elle permet, en effet, de diviser par 3 votre facture de chauffage. Ces économies font de la pompe à chaleur air-air un système rentable en seulement quelques années.</Typography>
+      <Typography variant='h4' color='primary'>Comment fonctionne une PAC air-air ?</Typography>
+      <Typography>Le principe de fonctionnement de la pompe à chaleur air-air est simple. L’appareil capte les calories présentes dans l’air extérieur pour chauffer un logement. Pour fonctionner, la pompe à chaleur a besoin de deux unités. L’une, située à l’extérieur, récupère les calories de l’air et l’autre, à l’intérieur, la diffuse au sein du logement grâce à un ventilo-convecteur.</Typography>
+      <Typography variant='h4' color='primary'>Vos factures de chauffage divisées par 3 !​​</Typography>
+      <Typography>
+En utilisant l’énergie gratuite de l’air, vous ne payez que l'électricité nécessaire à la circulation du fluide caloporteur pour faire fonctionner la pompe à chaleur. Vous pouvez ainsi diviser vos factures de chauffage par 3. Ces économies permettent d’amortir en seulement quelques années le prix de la pompe à chaleur air-air qui varie entre 1.600 € et 2.800 € par unité intérieure installée.</Typography>
+      <Typography variant='h4' color='primary'>​​​Puis-je installer une PAC air-air chez moi ?</Typography>
+      <Typography>L’installation d’une pompe à chaleur air-air est très simple. En effet, il suffit de disposer d'un jardin ou d’une cour pour y installer l'unité extérieure. Un conseil : choisissez un emplacement qui occasionne le moins de bruit pour le voisinage (distance minimale, éviter les angles et recoins qui renvoient le son). Il vous faut aussi un emplacement dans votre logement pour installer les unités intérieures. Les ventilo-convecteurs s'installent simplement au-dessus des portes des pièces à chauffer. La jonction entre les deux est assurée par un circuit de fluide frigorigène.</Typography>
+      <Typography variant='h4' color='primary'>ATTENTION IMPORTANT !!</Typography>
+      <Typography fontWeight='bold'>Pour bénéficier des différentes aides mises en place par l’Etat , telles que celles de l’Anah ou les primes énergie, l’installation de votre PAC air-air doit être réalisée par un professionnel certifié Reconnu Garant de l’Environnement.</Typography>
+      <div style={{ width: '100%', textAlign: 'center', marginTop: '50px' }}>
+        <Button size='large' variant='contained' onClick={() => { window.location = '/#form' }}>Je teste mon éligibilité</Button>
+      </div>
+    </Container>
   )
 }
 
--- src/pages/PACAirEau.js
+++ src/pages/PACAirEau.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Button, Container, Typography } from '@mui/material'
+import video from '../assets/videos/pac.mp4'
 
 function PACAirEau() {
   return (
-    <div>PACAirEau</div>
+    <Container>
+      <Typography variant='h3' color='primary' fontWeight='bold' textAlign='center' className='my-5'>Pompe à chaleur Air/Eau</Typography>
+      <div style={{textAlign: 'center'}}><video src={video} controls loop autoPlay muted></video></div>
+      <Typography variant='h4' color='primary'>Tout savoir sur la pompe à chaleur Air-Eau</Typography>
+      <Typography fontStyle='italic'>La pompe à chaleur air-air utilise les calories de l’air, une énergie gratuite, pour chauffer efficacement et à moindre coût votre logement. Elle permet, en effet, de diviser par 3 votre facture de chauffage. Ces économies font de la pompe à chaleur air-air un système rentable en seulement quelques années.</Typography>
+      <Typography variant='h5' color='primary'>Comment fonctionne une PAC air-eau ?</Typography>
+      <Typography>A la différence de la pompe à chaleur air-air qui puise l'énergie dans l'air extérieur et la restitue sous forme d’air chaud, la pompe à chaleur air-eau, puise les calories dans l’air extérieur mais utilise l'eau pour transporter la chaleur. La plupart des modèles cessent de fonctionner dès que la température descend sous les -5°C, rendant indispensable un chauffage d’appoint dans certaines régions. Néanmoins, certains modèles, plus performants peuvent être opérationnels jusqu’à -25°C. Les performances de votre pompe à chaleur dépendent donc du climat de votre région. Cette solution n’est pas la plus adaptée lorsque votre région est soumise à des hivers rigoureux et des températures négatives. En effet, plus vous sollicitez l’électricité pour réchauffer l’eau de chauffage de votre installation, moins le rendement de votre pompe à chaleur air-eau est important et plus vous consommez d’énergie.</Typography>
+      <img src="" alt="" />
+      <Typography variant='h5' color='primary'>La pompe à chaleur air-eau se compose de quatre composants</Typography>
+      <Typography>La pompe à chaleur air-eau se présente, pour la plupart des modèles, sous la forme d'un monobloc. Elle se compose de 4 éléments :
+        <li>L’évaporateur qui permet la transformation du fluide frigorigène en vapeur ;</li>
+        <li>Le compresseur qui augmente la pression et la chaleur du gaz ;</li>
+        <li>Le condenseur qui permet au fluide frigorigène de restituer la chaleur à l’eau de chauffage ;</li>
+        <li>Le détendeur qui diminue la pression et la température du gaz.</li></Typography>
+      <Typography variant='h5' color='primary'>Le rendement de la pompe à chaleur air-eau</Typography>
+      <Typography>Pour produire du chauffage, la pompe à chaleur air-eau consomme de l’électricité. Elle a notamment besoin d’un moteur électrique pour compresser le fluide frigorigène. Le rendement d’une pompe à chaleur est exprimé par son COP (Coefficient de performance). Il traduit la quantité de chaleur produite par rapport à l’énergie électrique consommée par le compresseur. Plus le COP est élevé, plus la pompe à chaleur est performante. Une pompe à chaleur air-eau ayant un coefficient de performance (COP) de 3 permet de restituer 3 kWh d'énergie thermique pour 1 kWh d'énergie électrique consommée.</Typography>
+      <Typography variant='h4' color='primary'>ATTENTION IMPORTANT !!</Typography>
+      <Typography fontWeight='bold'>Pour bénéficier des différentes aides mises en place par l’Etat , telles que celles de l’Anah ou les primes énergie, l’installation de votre PAC air-air doit être réalisée par un professionnel certifié Reconnu Garant de l’Environnement.</Typography>
+      <div style={{ width: '100%', textAlign: 'center', marginTop: '50px' }}>
+        <Button size='large' variant='contained' onClick={() => { window.location = '/#form' }}>Je teste mon éligibilité</Button>
+      </div>
+    </Container>
   )
 }
 
